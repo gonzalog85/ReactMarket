554497d0043709530723d2bb33c5fa6e8bb70fc3
Gonzalo <gonzalogomez22@gmail.com>
2021-08-09 17:43:31 -0300

    Componente Login, Registro usuario

---
+++ src/componentes/seguridad/RegistrarUsuario.js
@@ -0,0 +1,74 @@
+import {
+    Avatar,
+    Button,
+    Card,
+    Container,
+    Grid,
+    Icon,
+    Link,
+    TextField,
+    Typography,
+} from '@material-ui/core';
+import React from 'react';
+import useStyles from '../../theme/useStyles';
+
+const RegistrarUsuario = () => {
+    const classes = useStyles();
+    return (
+        <Container className={classes.containermt}>
+            <Grid container justify="center">
+                <Grid item lg={6} md={8}>
+                    <Card className={classes.card} align="center">
+                        <Avatar className={classes.avatar}>
+                            <Icon className={classes.icon}>person_add</Icon>
+                        </Avatar>
+                        <Typography variant="h5" color="primary">
+                            Registrar Usuario
+                        </Typography>
+                        <form className={classes.form}>
+                            <Grid container spacing={2}>
+                                <Grid item lg={6} xs={12} className={classes.gridmb}>
+                                    <TextField label="Nombre" variant="outlined" fullWidth />
+                                </Grid>
+
+                                <Grid item lg={6} xs={12} className={classes.gridmb}>
+                                    <TextField label="Apellidos" variant="outlined" fullWidth />
+                                </Grid>
+
+                                <Grid item lg={12} xs={12} className={classes.gridmb}>
+                                    <TextField
+                                        label="Email"
+                                        variant="outlined"
+                                        fullWidth
+                                        type="email"
+                                    />
+                                </Grid>
+
+                                <Grid item lg={12} xs={12} className={classes.gridmb}>
+                                    <TextField
+                                        label="Password"
+                                        variant="outlined"
+                                        fullWidth
+                                        type="password"
+                                    />
+                                </Grid>
+
+                                <Grid item lg={12} xs={12} className={classes.gridmb}>
+                                    <Button variant="contained" fullWidth color="primary">
+                                        Registrar
+                                    </Button>
+                                </Grid>
+                            </Grid>
+
+                            <Link href="/" variant="body1" className={classes.link}>
+                                ¿Ya tienes cuenta?. Logeate
+                            </Link>
+                        </form>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default RegistrarUsuario;
